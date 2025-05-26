a094e74b5964ef0fca65338ee519687d722b6c86
Nens01 <neneeoguchi@icloud.com>
2025-05-26 14:34:32 -0600

    HTML + CSS

---
+++ javascript/index.js
@@ -0,0 +1,5 @@
+//Los Angeles
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngeles.querySelector(".date");
+losAngelesDateElement.innerHTML = "May 26, 2025";
+losAngelesDateElement.innerHTML = "12:00:15 <small> PM</small>";
