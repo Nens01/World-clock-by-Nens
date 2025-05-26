942a07da8cc82fad39dee1588aaf687667da93c9
Nens01 <neneeoguchi@icloud.com>
2025-05-26 17:29:23 -0600

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -2,5 +2,19 @@
 let losAngelesElement = document.querySelector("#los-angeles");
 let losAngelesDateElement = losAngelesElement.querySelector(".date");
 let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-losAngelesDateElement.innerHTML = moment().format("dddd", "MMMM", "Do", "YYYY");
-losAngelesTimeElement.innerHTML = moment().format("HH:mm:ss A");
+losAngelesDateElement.innerHTML = moment()
+  .tz("America/losAngeles")
+  .format("dddd", "MMMM", "Do", "YYYY");
+losAngelesTimeElement.innerHTML = moment()
+  .tz("America/losAngeles")
+  .format("HH:mm:ss A");
+//New York
+let newYorkElement = document.querySelector("#new-york");
+let newYorkDateElement = newYorkElement.querySelector(".date");
+let newYorkTimeElement = newYorkElement.querySelector(".time");
+newYorkDateElement.innerHTML = moment()
+  .tz("America/New_York")
+  .format("dddd, MMMM Do YYYY");
+newYorkTimeElement.innerHTML = moment()
+  .tz("America/New_York")
+  .format("HH:mm:ss A");
