290e50e63c872b62166b5c96d838bb8641df9918
Nens01 <neneeoguchi@icloud.com>
2025-05-26 18:38:19 -0600

    html + css + JS

--- javascript/index.js
+++ javascript/index.js
@@ -1,20 +1,24 @@
-//Los Angeles
-let losAngelesElement = document.querySelector("#los-angeles");
-let losAngelesDateElement = losAngelesElement.querySelector(".date");
-let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-losAngelesDateElement.innerHTML = moment()
-  .tz("America/Los_Angeles")
-  .format("dddd, MMMM Do YYYY");
-losAngelesTimeElement.innerHTML = moment()
-  .tz("America/Los_Angeles")
-  .format("HH:mm:ss A");
-//New York
-let newYorkElement = document.querySelector("#new-york");
-let newYorkDateElement = newYorkElement.querySelector(".date");
-let newYorkTimeElement = newYorkElement.querySelector(".time");
-newYorkDateElement.innerHTML = moment()
-  .tz("America/New_York")
-  .format("dddd, MMMM Do YYYY");
-newYorkTimeElement.innerHTML = moment()
-  .tz("America/New_York")
-  .format("HH:mm:ss A");
+function updateClock() {
+  //Los Angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  losAngelesDateElement.innerHTML = moment()
+    .tz("America/Los_Angeles")
+    .format("dddd, MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = moment()
+    .tz("America/Los_Angeles")
+    .format("HH:mm:ss A");
+  //New York
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  newYorkDateElement.innerHTML = moment()
+    .tz("America/New_York")
+    .format("dddd, MMMM Do YYYY");
+  newYorkTimeElement.innerHTML = moment()
+    .tz("America/New_York")
+    .format("HH:mm:ss A");
+}
+updateClock();
+setInterval(updateClock, 1);
